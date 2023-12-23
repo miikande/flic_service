8a75804adfa0ad6835340e47ee3797bacddec96e
Miikka Andersson <miikka.andersson@gmail.com>
2023-12-23 22:14:22 +0200

    Update

--- flic_event_listener.py
+++ flic_event_listener.py
@@ -11,14 +11,17 @@ server_ip = "127.0.0.1"
 client = fliclib.FlicClient(server_ip)
 
 def got_button(bd_addr):
-	cc = fliclib.ButtonConnectionChannel(bd_addr)
-	cc.on_button_up_or_down = \
-		lambda channel, click_type, was_queued, time_diff: \
-			print(channel.bd_addr + " " + str(click_type))
-	cc.on_connection_status_changed = \
-		lambda channel, connection_status, disconnect_reason: \
-			print(channel.bd_addr + " " + str(connection_status) + (" " + str(disconnect_reason) if connection_status == fliclib.ConnectionStatus.Disconnected else ""))
-	client.add_connection_channel(cc)
+    cc = fliclib.ButtonConnectionChannel(bd_addr)
+ 
+    # Drop queued button click events
+    cc.on_button_click = \
+        lambda channel, click_type, was_queued, time_diff: \
+            print(channel.bd_addr + " " + str(click_type)) if not was_queued else None
+            
+    cc.on_connection_status_changed = \
+        lambda channel, connection_status, disconnect_reason: \
+            print(channel.bd_addr + " " + str(connection_status) + (" " + str(disconnect_reason) if connection_status == fliclib.ConnectionStatus.Disconnected else ""))
+    client.add_connection_channel(cc)
 
 def got_info(items):
 	print(items)
